52da7a4b69bff8fd0747dfe05a874ae64b1cb1d5
Nataliia Tsirul <tcirulnatalya@gmail.com>
2023-10-05 20:23:59 +0100

    added single pdouct and shop page

--- script.js
+++ script.js
@@ -13,3 +13,21 @@ if (close) {
     nav.classList.remove('active');
   });
 }
+
+const MainImg = document.getElementById('MainImg');
+const smallImg = document.getElementsByClassName('small-img');
+
+smallImg[0].onclick = function () {
+  MainImg.src = smallImg[0].src;
+};
+smallImg[1].onclick = function () {
+  MainImg.src = smallImg[1].src;
+};
+smallImg[2].onclick = function () {
+  MainImg.src = smallImg[2].src;
+};
+smallImg[3].onclick = function () {
+  MainImg.src = smallImg[3].src;
+};
+
+
